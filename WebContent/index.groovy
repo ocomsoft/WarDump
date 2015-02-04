6edb689fa8e202bbfe2858274fe020c180ee9957
Scott Warren <scottwarren@ocom.com.au>
2015-02-04 12:51:59 +1100

    Bootstrap layout and recursively dump data

--- WebContent/index.groovy
+++ WebContent/index.groovy
@@ -8,21 +8,39 @@ if (params.newSession) {
 	session = request.getSession(true)
 }
 
-def dumpMap (parentElement, String name, Map data) {
+def dumpValue (parentElement, prop, value, level) {	
+	if (value instanceof Map) {
+		parentElement.td {
+			dumpMap(delegate, prop, value, level+1)
+		}
+	}
+	else if (value instanceof Collection) {
+		parentElement.td {
+			dumpCollection(delegate, prop, value, level+1)
+		}
+	}
+	else {
+		parentElement.td  value.toString() 
+	}
+}
+def dumpMap (parentElement, String name, Map data, Integer level) {
+	def hElement = "h${level}" 
 	parentElement.div {
-		h2 name
-		table {
-			thead {
+		a (id:name) {
+			"$hElement" (name)
+		}
+		table (class:"table table-bordered") {
+			thead (class:"") {
 				tr {
-					td "Parameter"
-					td "Value"
+					th (class:"col-md-2", "Parameter")					
+					th "Value"
 				}
 			}
 			tbody {
 				data.each {prop, value ->
 					tr {
 						td prop
-						td value
+						dumpValue (delegate, prop, value, level)
 					}
 				}
 			}
@@ -30,28 +48,70 @@ def dumpMap (parentElement, String name, Map data) {
 	}
 }
 		
-
+def dumpCollection (parentElement, String name, Collection data, Integer level) {
+	def hElement = "h${level}"
+	parentElement.div {
+		a (id:name) {
+			"$hElement" (name)
+		}
+		table (class:"table table-bordered") {
+			thead (class:"") {
+				tr {
+					th (class:"col-md-2", "Index")
+					th "Value"
+				}
+			}
+			tbody {
+				data.eachWithIndex {value, index ->
+					tr {
+						td index			
+						dumpValue (delegate, index, value, level)						
+					}
+				}
+			}
+		}
+	}
+}
 
 html.html {
 	head {
 		title "Hello from Server "
+		link  (href:"//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css", rel:"stylesheet")
 	}
 	body {
 		h1 "Hello ${context.getInitParameter('Version')}!"
 		
 		h2 "Session"
 		p "Session ID: ${session.id}"
-		a (href:request.requestURI+"?newSession=true", "New Session")
+		a (href:request.requestURI+"?newSession=true", "New Session")			
 		
-		dumpMap (delegate, "Request Dump", request.properties)
+		div (class:"row") {
+			div (class:"col-sm-2") {
+				div (class:"list-group") {
+					a (class:"list-group-item", href:"#Request Dump", "Request Map")
+					a (class:"list-group-item", href:"#Session Dump", "Session Map")
+					a (class:"list-group-item", href:"#Parameters Dump", "Parameters Map")
+					a (class:"list-group-item", href:"#Parameters Dump", "Parameters Map")
+					a (class:"list-group-item", href:"#Headers Dump", "Headers Map")
+					a (class:"list-group-item", href:"#Context Dump", "Context Map")
+					a (class:"list-group-item", href:"#Environment Vars", "Environment Vars")
+				}
+			}			
+			div (class:"col-sm-10") {
+				dumpMap (delegate, "Request Dump", request.properties, 1)
+				
+				dumpMap (delegate, "Session Dump", session.properties, 1)
+				
+				dumpMap (delegate, "Parameters Dump", params, 1)
+				
+				dumpMap (delegate, "Headers Dump", headers, 1)
+				
+				dumpMap (delegate, "Context Dump", context.properties, 1)
+				
+				dumpMap (delegate, "Environment Vars", System.getenv(), 1)
+			}
+		}
 		
-		dumpMap (delegate, "Session Dump", session.properties)
-		
-		dumpMap (delegate, "Parameters Dump", params)
-		
-		dumpMap (delegate, "Headers Dump", headers)
-		
-		dumpMap (delegate, "Context Dump", context.properties)
 		
 	}
 }
